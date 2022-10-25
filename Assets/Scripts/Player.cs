0f829f451d242c36910fb1c24d01c62a7e590976
10-BryanOng <bryanong0102@yahoo.com.sg>
2022-10-25 12:01:25 +0800

    Added GameOver
    Added GameOver Conditions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,20 +5,43 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
-    public float Speed;
+
+    public int JumpSpeed = 30;
+    Rigidbody rb;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.position = transform.up * Speed * Time.deltaTime;
-        }
+            rb.velocity = Vector2.up * JumpSpeed;
             thisAnimation.Play();
+        }
+        
+        //Maximum Height
+        if(transform.position.y >= 3.6)
+        {
+            transform.position = new Vector3(transform.position.x, 3.6f, 0);
+        }
+
+        //GameOver
+        else if(transform.position.y <= -4.3)
+        {
+            GameManager.thisManager.GameOver();
+        }
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Obstacle")
+        {
+            GameManager.thisManager.GameOver();
+        }
     }
 }
