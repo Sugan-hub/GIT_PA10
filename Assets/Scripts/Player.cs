48b46dbcd6ef59e4e224b136d3783093efc8e624
qihang <qihangpro@gmail.com>
2023-10-17 16:06:50 +0800

    new commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
 
+    public Rigidbody rb;
+    public float force;
+    
+
+    public float minY = -3.5f;
+    public float maxY = 3.5f;
+
+    bool triggeredPoints;
+
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
-        thisAnimation["Flap_Legacy"].speed = 3;
+        thisAnimation["Flap_Legacy"].speed = 3;       
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+        }
+
+        Vector3 currentPosition = transform.position;
+        currentPosition.y = Mathf.Clamp(currentPosition.y, minY, maxY);
+        transform.position = currentPosition;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle") && !triggeredPoints)
+        {
+            triggeredPoints = true;
+            GameManager.thisManager.UpdateScore(1);
+        }
+    }
+
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Obstacle") && triggeredPoints)
+        {
+            triggeredPoints = false;
+        }
+    }
+
 }
